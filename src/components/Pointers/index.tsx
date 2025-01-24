ba41347afa68f093123d2432708d49b884c3088b
David Carr <daviddcarr13@gmail.com>
2025-01-23 22:06:28 -0600

    Improve design for mobile and in light and dark mode.

--- src/components/Pointers/index.tsx
+++ src/components/Pointers/index.tsx
@@ -30,56 +30,61 @@ const Pointers = ({
     parentMounted
 }: PointersProps) => {
 
-
-    let combined: DualPokeDamageRelations | null = null;
+    // If Defending and Dual Type has value
     if (battlePosition === "from" && selectedDualType && selectedType !== selectedDualType) {
-        combined = combineDualTypes(selectedType, selectedDualType)
-    }
+        let combined: DualPokeDamageRelations = combineDualTypes(selectedType, selectedDualType)
 
-    return (
-        <>
+        return (
             <div className="absolute left-1/2 top-1/2 w-0 h-0 ">
-                {/* Pointer Map */}
                 {
-                    battlePosition === "from" && combined !== null ? 
-                        DUAL_DAMAGE_TYPES.map((damageType, damageIndex) => {
-                            const damage_index: DualDamageRelationKey = `${damageType}_damage_from` as DualDamageRelationKey
-                            const matchingTypes: PokeType[] = combined[damage_index]
-    
-                            return matchingTypes.map((type, index) => {
-                                return (
-                                    <Pointer 
-                                        key={`${damageIndex}_${index}`}
-                                        type={getDataFromType(type)}
-                                        radius={radius}
-                                        position={battlePosition}
-                                        damageType={damageType}
-                                        parentMounted={parentMounted}
-                                        />
-                                )
+                    DUAL_DAMAGE_TYPES.map((damageType, damageIndex) => {
+                        const damage_index: DualDamageRelationKey = `${damageType}_damage_from` as DualDamageRelationKey
+                        const matchingTypes: PokeType[] = combined[damage_index]
+
+                        return matchingTypes.map((type, index) => {
+                            return (
+                                <Pointer 
+                                    key={`${damageIndex}_${index}`}
+                                    type={getDataFromType(type)}
+                                    radius={radius}
+                                    position={battlePosition}
+                                    damageType={damageType}
+                                    parentMounted={parentMounted}
+                                    />
+                            )
                         })
-                        })
-                     : DAMAGE_TYPES.map((damageType, damageIndex) => {
-                            const damage_index: DamageRelationKey = `${damageType}_damage_${battlePosition}` as DamageRelationKey
-                            const matchingTypes: PokeType[] = selectedType.damage_relations[damage_index]
-    
-                            return matchingTypes.map((type, index) => {
-                                return (
-                                    <Pointer 
-                                        key={`${damageIndex}_${index}`}
-                                        type={getDataFromType(type)}
-                                        radius={radius}
-                                        position={battlePosition}
-                                        damageType={damageType}
-                                        parentMounted={parentMounted}
-                                        />
-                                )
-                            })
-                        })
+                    })
                 }
             </div>
-        </>
-    )
+        )
+
+    // Default Pointers
+    } else {
+
+        return (
+            <div className="absolute left-1/2 top-1/2 w-0 h-0 ">
+                {
+                    DAMAGE_TYPES.map((damageType, damageIndex) => {
+                        const damage_index: DamageRelationKey = `${damageType}_damage_${battlePosition}` as DamageRelationKey
+                        const matchingTypes: PokeType[] = selectedType.damage_relations[damage_index]
+
+                        return matchingTypes.map((type, index) => {
+                            return (
+                                <Pointer 
+                                    key={`${damageIndex}_${index}`}
+                                    type={getDataFromType(type)}
+                                    radius={radius}
+                                    position={battlePosition}
+                                    damageType={damageType}
+                                    parentMounted={parentMounted}
+                                    />
+                            )
+                        })
+                    })
+                }
+            </div>
+        )
+    }
 }
 
 export default Pointers
