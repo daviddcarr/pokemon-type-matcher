66d4cff30d28dd6fcd9b35bb403d72da546ac8c5
David Carr <daviddcarr13@gmail.com>
2025-01-28 14:25:50 -0600

    Add vite-pwa and make app installable on devices.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import fs from 'fs'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const manifestPath = path.resolve(__dirname, 'public/manifest.json')
+const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'))
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest,
+      
+    })
+  ],
   resolve: {
     alias: {
       '@data': path.resolve(__dirname, './src/data'),
