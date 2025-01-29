82c761068838de3b5f1712e0da99f748842fadc2
David Dylan Carr <daviddcarr13@gmail.com>
2025-01-29 17:37:38 -0600

    Update particles in shader so that they spread out across the width of device.

--- src/shaders/background/fragment.glsl
+++ src/shaders/background/fragment.glsl
@@ -4,39 +4,38 @@ uniform vec2 iResolution;
 uniform float iTime;
 uniform sampler2D typeIcon;
 
-float get_particle_x(int i) {
-    return (sin(float(i)) * 0.5) * (sin(float(i) * 6.0) + 0.5) * 1.75 + 0.5;
+float random(vec2 st)
+{
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
 }
 
 void main() {
+    // UV Alterations
     vec2 uv = vUV;
     vec2 aspectCorrectedUV = uv;
-
     aspectCorrectedUV.x *= iResolution.x / iResolution.y;
-
     vec2 flippedUV = vec2(aspectCorrectedUV.x, 1.0 - aspectCorrectedUV.y);
 
-    vec3 col = vec3(0.0);
-
+    // Wave Mask
     float sine = sin(uv.x * 2.0 + (iTime * 0.2)) * 0.1 + 0.2;
     float sine_2 = sin(uv.x * 5.0 + (iTime * 0.5)) * 0.05;
     float sine_3 = sin(uv.x * 12.0 + (iTime * 1.1)) * 0.02;
     float mask = step(uv.y, sine + sine_2 + sine_3);
 
-
-    float finalMask = mask;
     for ( int i = 1; i < 10; i++) {
-        float particle_x = get_particle_x(i);
-        float time_offset = get_particle_x(i + 2) * 40.0;
-        float speed_offset = get_particle_x(i + 3) * 0.05;
-        float scale_factor = get_particle_x(i + 5) * 50.0;
+        float rand_x = random(vec2(float(i), float(i)));
+        rand_x *= (iResolution.x / iResolution.y);
 
-        vec2 floatingUV = vec2(flippedUV.x - abs(particle_x), fract(flippedUV.y + ((iTime + abs(time_offset)) * abs(speed_offset))));
-        vec4 textureColor = texture2D(typeIcon, floatingUV * min(15.0, scale_factor));
+        float time_offset = random(vec2(float(i + 2), float(i + 2))) * 40.0;
+        float speed_offset = max(0.02, random(vec2(float(i + 3), float(i + 3))) * 0.2);
+        float scale_factor = min(15.0, random(vec2(float(i + 5), float(i + 5))) * 200.0);
 
-        finalMask = max(finalMask, textureColor.r);
+        vec2 floatingUV = vec2(flippedUV.x - (rand_x), fract(flippedUV.y + ((iTime + abs(time_offset)) * abs(speed_offset))));
+        vec4 textureColor = texture2D(typeIcon, floatingUV * scale_factor);
+
+        mask = max(mask, textureColor.r);
     }
 
-    gl_FragColor = vec4(vec3(0.0), finalMask);
+    gl_FragColor = vec4(vec3(0.0, 0.0, 0.1), mask);
     
 }
